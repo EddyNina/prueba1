0ba5ed5a8ea590b8d22a14e9ba0c5f36363edee0
EddyNina <49159462+EddyNina@users.noreply.github.com>
2019-04-02 12:12:38 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace como_estan_utesianos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("hola utesianos");
+            Console.ReadKey();
+        }
+    }
+}
